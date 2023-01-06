52cf2f2ec8e720099189098683c6ba8026725e7c
Joel Wurtz <joel.wurtz@gmail.com>
2023-01-07 00:02:50 +0100

    feat(server): split tcp and udp server into their own files

---
+++ src/server/mod.rs
@@ -0,0 +1,5 @@
+mod tcp;
+mod udp;
+
+pub use tcp::create_tcp_server;
+pub use udp::create_udp_server;
--- src/server/udp.rs
+++ src/server/udp.rs
@@ -1,14 +1,8 @@
-use crate::client::Client;
 use crate::error::DecryptError;
-use crate::handler::MessageHandler;
 use crate::message::ClientMessage;
-use crate::proto::mumble::Version;
-use crate::proto::MessageKind;
 use crate::sync::RwLock;
 use crate::voice::VoicePacket;
 use crate::ServerState;
-use actix_server::Server;
-use actix_service::fn_service;
 use anyhow::Context;
 use byteorder::{ReadBytesExt, WriteBytesExt};
 use bytes::BytesMut;
@@ -16,132 +10,7 @@ use std::collections::HashMap;
 use std::io::Cursor;
 use std::sync::Arc;
 use std::time::Instant;
-use tokio::io;
-use tokio::io::ReadHalf;
-use tokio::net::{TcpListener, TcpStream, UdpSocket};
-use tokio::sync::mpsc;
-use tokio::sync::mpsc::Receiver;
-use tokio_rustls::{server::TlsStream, TlsAcceptor};
-
-pub fn create_tcp_server(
-    tcp_listener: TcpListener,
-    acceptor: TlsAcceptor,
-    server_version: Version,
-    state: Arc<RwLock<ServerState>>,
-) -> Server {
-    Server::build()
-        .listen(
-            "mumble-tcp",
-            tcp_listener.into_std().expect("cannot create tcp listener"),
-            move || {
-                let acceptor = acceptor.clone();
-                let server_version = server_version.clone();
-                let state = state.clone();
-
-                fn_service(move |stream: TcpStream| {
-                    let acceptor = acceptor.clone();
-                    let server_version = server_version.clone();
-                    let state = state.clone();
-
-                    stream.set_nodelay(true).unwrap();
-
-                    async move {
-                        let mut stream = acceptor.accept(stream).await.map_err(|e| {
-                            tracing::error!("accept error: {}", e);
-
-                            e
-                        })?;
-
-                        let (version, authenticate, crypt_state) = Client::init(&mut stream, server_version).await.map_err(|e| {
-                            tracing::error!("init client error: {}", e);
-
-                            e
-                        })?;
-
-                        let (read, write) = io::split(stream);
-                        let (tx, rx) = mpsc::channel(32);
-
-                        let username = authenticate.get_username().to_string();
-                        let client = {
-                            state.write_err().await.context("failed to add client")?.add_client(
-                                version,
-                                authenticate,
-                                crypt_state,
-                                write,
-                                tx,
-                            )
-                        };
-
-                        crate::metrics::CLIENTS_TOTAL.inc();
-
-                        tracing::info!("new client {} connected", username);
-
-                        match client_run(read, rx, state.clone(), client.clone()).await {
-                            Ok(_) => (),
-                            Err(e) => tracing::error!("client {} error: {:?}", username, e),
-                        }
-
-                        tracing::info!("client {} disconnected", username);
-
-                        crate::metrics::CLIENTS_TOTAL.dec();
-
-                        {
-                            state.write_err().await.context("disconnect user")?.disconnect(client).await?;
-                        }
-
-                        Ok::<(), anyhow::Error>(())
-                    }
-                })
-            },
-        )
-        .expect("cannot create tcp server")
-        .run()
-}
-
-pub async fn client_run(
-    mut read: ReadHalf<TlsStream<TcpStream>>,
-    mut receiver: Receiver<ClientMessage>,
-    state: Arc<RwLock<ServerState>>,
-    client: Arc<RwLock<Client>>,
-) -> Result<(), anyhow::Error> {
-    let codec_version = { state.read_err().await?.check_codec().await? };
-
-    if let Some(codec_version) = codec_version {
-        {
-            client
-                .read_err()
-                .await?
-                .send_message(MessageKind::CodecVersion, &codec_version)
-                .await?;
-        }
-    }
-
-    {
-        let client_sync = client.read_err().await?;
-
-        client_sync.sync_client_and_channels(&state).await.map_err(|e| {
-            tracing::error!("init client error during channel sync: {:?}", e);
-
-            e
-        })?;
-        client_sync.send_my_user_state().await?;
-        client_sync.send_server_sync().await?;
-        client_sync.send_server_config().await?;
-    }
-
-    let user_state = { client.read_err().await?.get_user_state() };
-
-    {
-        match state.read_err().await?.broadcast_message(MessageKind::UserState, &user_state).await {
-            Ok(_) => (),
-            Err(e) => tracing::error!("failed to send user state: {:?}", e),
-        }
-    }
-
-    loop {
-        MessageHandler::handle(&mut read, &mut receiver, state.clone(), client.clone()).await?
-    }
-}
+use tokio::net::UdpSocket;
 
 pub async fn create_udp_server(protocol_version: u32, socket: Arc<UdpSocket>, state: Arc<RwLock<ServerState>>) {
     loop {
