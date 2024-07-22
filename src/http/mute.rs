d75ba2e74df296b5dff18dc890bc25eb4ee6e435
Joel Wurtz <joel.wurtz@gmail.com>
2024-07-23 00:09:42 +0200

    feat(zumble): add mute all

--- src/http/mute.rs
+++ src/http/mute.rs
@@ -13,6 +13,11 @@ pub struct Mute {
     user: String,
 }
 
+#[derive(Serialize, Deserialize)]
+pub struct MuteAll {
+    mute: bool,
+}
+
 pub async fn post_mute(State(state): State<AppStateRef>, Json(mute): Json<Mute>) -> StatusCode {
     if let Some(client) = state.server.get_client_by_name(mute.user.as_str()).await {
         client.set_mute(mute.mute);
@@ -35,4 +40,10 @@ pub async fn get_mute(Path(username): Path<String>, State(state): State<AppState
     }
 
     Err(StatusCode::NOT_FOUND)
+}
+
+pub async fn post_mute_all(State(state): State<AppStateRef>, Json(mute_all): Json<MuteAll>) -> StatusCode {
+    state.server.mute_all.store(mute_all.mute, std::sync::atomic::Ordering::Relaxed);
+
+    StatusCode::OK
 }
