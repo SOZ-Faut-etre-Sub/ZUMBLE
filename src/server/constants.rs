8d205a2fa44e97b733ecedfe7286f028d4ea8201
AvarianKnight <dillon@dillonskaggs.dev>
2024-12-03 12:59:19 -0600

    fix: properly reject players if over max client limit
    - increases buffer size for TCP packets (not entirely sure if this is needed)

---
+++ src/server/constants.rs
@@ -0,0 +1,7 @@
+/// The amount of players the server can support
+pub const MAX_CLIENTS: u32 = 4096;
+
+/// the bandwidth (in bytes) that the client can use
+pub const MAX_BANDWIDTH: u32 = 144000;
+
+pub const MAX_MTU: usize = 1400;
--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,5 +1,6 @@
 mod tcp;
 mod udp;
+pub mod constants;
 
 pub use tcp::create_tcp_server;
 pub use udp::create_udp_server;
