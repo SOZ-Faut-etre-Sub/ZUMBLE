9fbf13f5f5d7f00f297d5619ac96b3dc69a6bc9c
Dillon Skaggs <dillon@dillonskaggs.dev>
2024-12-03 11:00:27 -0600

    tweak: swap VoiceTarget to use DashSets instead of RwLocks

--- src/target.rs
+++ src/target.rs
@@ -1,7 +1,7 @@
-use std::collections::HashSet;
+use dashmap::DashSet;
 
 #[derive(Default, Debug)]
 pub struct VoiceTarget {
-    pub sessions: HashSet<u32>,
-    pub channels: HashSet<u32>,
+    pub sessions: DashSet<u32>,
+    pub channels: DashSet<u32>,
 }
