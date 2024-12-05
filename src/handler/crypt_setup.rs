cb353f834b0b7dde2d21727761a3d220bc823d06
AvarianKnight <dillon@dillonskaggs.dev>
2024-12-05 02:01:00 -0600

    tweak: properly send errors down

--- src/handler/crypt_setup.rs
+++ src/handler/crypt_setup.rs
@@ -1,4 +1,4 @@
-use crate::client::{ClientRef};
+use crate::client::ClientRef;
 use crate::error::MumbleError;
 use crate::handler::Handler;
 use crate::proto::mumble::CryptSetup;
@@ -8,10 +8,9 @@ impl Handler for CryptSetup {
     async fn handle(&self, _state: ServerStateRef, client: ClientRef) -> Result<(), MumbleError> {
         if self.has_client_nonce() {
             client.crypt_state.lock().set_decrypt_nonce(self.get_client_nonce());
+            Ok(())
         } else {
-            client.send_crypt_setup(false).await?;
+            client.send_crypt_setup(false).await
         }
-
-        Ok(())
     }
 }
