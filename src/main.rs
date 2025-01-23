bacebbb7f1d82be7d72e763e34f1f1e14b46bb40
AvarianKnight <dillon@dillonskaggs.dev>
2025-01-23 15:42:19 -0600

    tweak: disable windows instead of by env
    - this is currently broken for `windows-gnu` but will still try to compile like its available, we instead just disable for all windows targets

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use rustls::ServerConfig;
 
-#[cfg(not(target_env = "msvc"))]
+#[cfg(not(target_os = "windows"))]
 use tikv_jemallocator::Jemalloc;
 
-#[cfg(not(target_env = "msvc"))]
+#[cfg(not(target_os = "windows"))]
 #[global_allocator]
 static GLOBAL: Jemalloc = Jemalloc;
 
